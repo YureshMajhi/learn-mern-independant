24fa5e8290b14026d62575e326c0b70f0e9ef77e
Yuresh Majhi <yureshmee@gmail.com>
2023-10-27 12:23:31 +0545

    Added login and signup page

---
+++ frontend independent/src/pages/Login.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    console.log(email, password);
+  };
+  return (
+    <form className="signup" onSubmit={handleSubmit}>
+      <h3>Login</h3>
+      <label htmlFor="email">Email: </label>
+      <input
+        type="email"
+        id="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <label htmlFor="password">Password: </label>
+      <input
+        type="password"
+        id="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <button>Sign up</button>
+    </form>
+  );
+};
+
+export default Login;
